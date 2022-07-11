e26996e5cbe100b6ac9aab934016566f32652fdf
karleene <karleenestanislao@gmail.com>
2022-07-11 13:27:09 -0500

    added our footer and links to our portfolio

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <h3>Created by beans at Juno College</h3>
+            <h3>Created by <a href="https://www.beatricecodes.com/">Beatrice</a>, <a href="https://noodlesandcodes.com/">Brian</a>, <a href="www.jaehyunshim.com">Jay</a> and <a href="htttps://www.karleene.com">Karleen</a> at <a href="https://wwww.junocollege.com">Juno College</a></h3>
         </footer>
     )
 }
