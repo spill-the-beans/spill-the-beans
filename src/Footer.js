0d861441b796311e99ebb034b8b47db8583a83c6
Brian Yoon <yoon.brian91@gmail.com>
2022-07-11 17:40:57 -0400

    links in the footer fixed

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <h3>Created by <a href="https://www.beatricecodes.com/">Beatrice</a>, <a href="https://noodlesandcodes.com/">Brian</a>, <a href="www.jaehyunshim.com">Jay</a> and <a href="htttps://www.karleene.com">Karleen</a> at <a href="https://wwww.junocollege.com">Juno College</a></h3>
+            <h3>Created by <a href="https://www.beatricecodes.com/">Beatrice</a>, <a href="https://noodlesandcodes.com/">Brian</a>, <a href="https://www.jaehyunshim.com">Jay</a> and <a href="https://www.karleene.com">Karleen</a> at <a href="https://www.junocollege.com">Juno College</a></h3>
         </footer>
     )
 }
